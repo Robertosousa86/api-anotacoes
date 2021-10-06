9f1932fcc6756f6a7d9e318b261f13d5b937f564
Roberto Sousa <robertosousa186@gmail.com>
2021-10-06 00:58:47 -0300

    Creating function list

--- www/init.js
+++ www/init.js
@@ -11,6 +11,7 @@ function save() {
       alert(data.message);
       $('#title').val('');
       $('#description').val('');
+      list();
     },
 
     error: function (res) {
@@ -18,3 +19,32 @@ function save() {
     },
   });
 }
+
+function list() {
+  $('.list').html('');
+  $.ajax({
+    type: 'get',
+    url: '/notes',
+    data: JSON.stringify({ title: title, description: description }),
+    contentType: 'application/json; charset=utf-8',
+    success: function (data) {
+      for (const note of data) {
+        $('.list').append(`
+    <div class="item">
+       <h2>${note.title}</h2>
+       <p>${note.description}</p>
+
+       <a href="">Editar</a>
+       <a href="">Excluir</a>
+    </div>
+        `);
+      }
+    },
+
+    error: function (res) {
+      alert(res.responseJSON.message);
+    },
+  });
+}
+
+list();
